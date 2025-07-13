6199ea4ea73ee7ca4a98268d440bbc334a4bf606
kannna5296 <kannnahacks5296@gmail.com>
2025-07-13 18:51:02 +0900

    型違いでボタン押せないバグ修正

--- src/views.py
+++ src/views.py
@@ -5,19 +5,20 @@ from tasks import TaskRepository
 from date_util import today_key
 
 class ConfirmView(View):
-    def __init__(self, user_id: str, task_repository: TaskRepository):
+    def __init__(self, authorized_user_id: str, task_repository: TaskRepository):
         super().__init__(timeout=86400)
-        self.user_id = user_id
+        self.authorized_user_id = authorized_user_id
         self.task_repository = task_repository
 
     @discord.ui.button(label="完了!", style=ButtonStyle.success)
     async def confirm(self, interaction: Interaction, button: Button):
-        if interaction.user.id != self.user_id:
+        clicked_user_id = str(interaction.user.id)
+        if  clicked_user_id != self.authorized_user_id:
             await interaction.response.send_message("これはあなた専用のボタンだよ！", ephemeral=True)
             return
 
         user_tasks = self.task_repository.load_tasks()
-        task = user_tasks.get(self.user_id, {}).get(today_key())
+        task = user_tasks.get(self.authorized_user_id, {}).get(today_key())
         if task:
             await interaction.response.send_message(f"🎉 お疲れ様！『{task}』完了したね！", ephemeral=True)
         else:
