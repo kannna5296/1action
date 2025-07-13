c69c0d849fde6e963eb15b110d495b3590286f8a
kannna5296 <kannnahacks5296@gmail.com>
2025-07-13 01:34:53 +0000

    Cron実行OK

--- bot.py
+++ bot.py
@@ -1,22 +1,41 @@
+import os
 import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
 
-# クライアント生成
+# Bot生成
 intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+load_dotenv()
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+# スケジューラ
+scheduler = AsyncIOScheduler()
+
+@bot.event
 async def on_ready():
-    print(f"We hav logged in as {client.user}")
+    print(f"1action get ready!!!")
+    scheduler.add_job(send_morning_message, CronTrigger(hour=7, minute=30))
+    scheduler.start()
 
-@client.event
-async def on_message(message):
-    """何か受信したメッセージに対して反応するアクション"""
-    #Bot自身からのメッセージには反応しない
-    if message.author == client.user :
-        return
+async def send_morning_message():
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send("おはようございます ☀️ 今日のやることをコメントしてね！")
 
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
+# @client.event
+# async def on_message(message):
+#     """何か受信したメッセージに対して反応するアクション"""
+#     #Bot自身からのメッセージには反応しない
+#     if message.author == client.user :
+#         return
 
-client.run('token')
+#     if message.content.startswith('hello'):
+#         await message.channel.send('Hello!')
+
+bot.run(TOKEN)
