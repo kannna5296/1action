675187110035a0709c1caa4fc5aa934b706460bb
kannna5296 <kannnahacks5296@gmail.com>
2025-07-15 23:41:27 +0900

    ログ追加

--- src/healthcheck.py
+++ src/healthcheck.py
@@ -1,6 +1,8 @@
 import threading
 from flask import Flask
 
+from logger import logger
+
 class HealthCheckServer:
     def __init__(self, host="0.0.0.0", port=8000):
         self.host = host
@@ -13,6 +15,7 @@ class HealthCheckServer:
             app = Flask(__name__)
             @app.route("/")
             def health():
+                logger.info(f"ヘルスチェックOK")
                 return "OK", 200
             app.run(host=self.host, port=self.port)
         self.thread = threading.Thread(target=run, daemon=True)
